f2f3524bd2715270cb624c6991b0a86980ec2ec4
Kirill Warp <kirill9990@gmail.com>
2020-12-14 00:44:07 +0300

    move lint and config to server

--- back/.eslintrc.js
+++ back/.eslintrc.js
@@ -1,5 +1,27 @@
 module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
-    '../.eslintrc.js',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'prettier/@typescript-eslint',
   ],
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+  },
+  rules: {
+    '@typescript-eslint/lines-between-class-members': 2,
+    '@typescript-eslint/no-unnecessary-type-assertion': 2,
+    '@typescript-eslint/prefer-nullish-coalescing': 2,
+    '@typescript-eslint/explicit-function-return-type': 2,
+    '@typescript-eslint/explicit-module-boundary-types': 2,
+    '@typescript-eslint/no-explicit-any': 2,
+  },
 };
