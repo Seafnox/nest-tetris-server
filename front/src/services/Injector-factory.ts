a75478d81eb41f7110cfebf045c11e436c3d8bfa
Kirill Warp <kirill9990@gmail.com>
2021-01-04 03:21:41 +0300

    create injector

---
+++ front/src/services/Injector-factory.ts
@@ -0,0 +1,25 @@
+export class InjectorFactory {
+  private static unit: InjectorService;
+
+  private static get(): InjectorService {
+    if (!InjectorFactory.unit) {
+      InjectorFactory.unit = new InjectorService();
+    }
+
+    return InjectorFactory.unit;
+  }
+}
+
+export class InjectorService {
+  private injectionMap = new Map();
+
+  public inject<T>(clazz: { new (injector: InjectorService): T }): T {
+    if (!this.injectionMap.has(clazz)) {
+      const unit = new clazz(this);
+
+      this.injectionMap.set(clazz, unit);
+    }
+
+    return this.injectionMap.get(clazz);
+  }
+}
