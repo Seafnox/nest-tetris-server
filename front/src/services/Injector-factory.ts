cddc721e06530e1f1d9b02e892844029fab26f06
Kirill Warp <kirill9990@gmail.com>
2021-01-04 04:06:51 +0300

    add informative injector error

--- front/src/services/Injector-factory.ts
+++ front/src/services/Injector-factory.ts
@@ -15,11 +15,19 @@ export class InjectorService {
 
   public inject<T>(clazz: { new (injector: InjectorService): T }): T {
     if (!this.injectionMap.has(clazz)) {
-      const unit = new clazz(this);
+      const unit = this.initialize(clazz);
 
       this.injectionMap.set(clazz, unit);
     }
 
     return this.injectionMap.get(clazz);
+  }
+
+  private initialize<T>(clazz: { new (injector: InjectorService): T }): T {
+    try {
+      return new clazz(this);
+    } catch (error) {
+      throw new Error(`${this.constructor.name}: ${error.message} (${typeof clazz} ${clazz?.constructor?.name})`);
+    }
   }
 }
