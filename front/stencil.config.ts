1299ea70a42e52a5bf2f7aa952062e2a4bed7692
Kirill Warp <kirill9990@gmail.com>
2021-01-05 17:45:32 +0300

    update global styles :fix:

--- front/stencil.config.ts
+++ front/stencil.config.ts
@@ -3,7 +3,7 @@ import { Config } from '@stencil/core';
 // https://stenciljs.com/docs/config
 
 export const config: Config = {
-  globalStyle: 'src/global/normalize.css',
+  globalStyle: 'src/global/app.css',
   globalScript: 'src/global/app.ts',
   taskQueue: 'async',
   outputTargets: [
