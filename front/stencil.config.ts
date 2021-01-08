b4212c881d7fe59e8dfecf43f687fad68bf1162e
Kirill Warp <kirill9990@gmail.com>
2021-01-09 02:14:16 +0300

    add plugin for socket io

--- front/stencil.config.ts
+++ front/stencil.config.ts
@@ -1,15 +1,22 @@
 import { Config } from '@stencil/core';
-
-// https://stenciljs.com/docs/config
+import nodePolyfills from 'rollup-plugin-node-polyfills';
 
 export const config: Config = {
+  rollupPlugins: {
+    after: [
+      nodePolyfills(),
+    ],
+  },
+  nodeResolve: {
+    browser: true,
+  },
   globalStyle: 'src/global/app.css',
   globalScript: 'src/global/app.ts',
   taskQueue: 'async',
   outputTargets: [
     {
       type: 'www',
-      // comment the following line to disable service workers in production
+// comment the following line to disable service workers in production
 //      serviceWorker: false,
 //      baseUrl: 'https://myapp.local/',
     },
