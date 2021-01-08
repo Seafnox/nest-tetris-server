65fee260394d01f13c443a3b0f9a434b569ba263
Kirill Warp <kirill9990@gmail.com>
2021-01-08 18:28:19 +0300

    simplify store names :additional:

--- front/src/services/state-controllers/client-sign-controller.ts
+++ front/src/services/state-controllers/client-sign-controller.ts
@@ -9,22 +9,22 @@ import { UserStore } from '../user-store';
 import { ClientStateController } from './client-state-controller';
 
 export class ClientSignController implements ClientStateController {
-  private readonly userStateService: UserStore;
+  private readonly userStore: UserStore;
   private readonly gameApiService: GameApiService;
   private readonly userNotificationsService: UserNotificationsService;
-  private readonly clientModeMediatorService: ClientModeStore;
+  private readonly clientModeStore: ClientModeStore;
   private readonly clientStateStore: ClientStateStore;
   private listenerId: symbol;
 
   constructor(injector: InjectorService) {
-    this.userStateService = injector.inject(UserStore);
+    this.userStore = injector.inject(UserStore);
     this.gameApiService = injector.inject(GameApiService);
     this.userNotificationsService = injector.inject(UserNotificationsService);
-    this.clientModeMediatorService = injector.inject(ClientModeStore);
+    this.clientModeStore = injector.inject(ClientModeStore);
     this.clientStateStore = injector.inject(ClientStateStore);
   }
   public start(): void {
-    this.listenerId = this.userStateService.addUserListener(user => {
+    this.listenerId = this.userStore.addUserListener(user => {
       if (!user || !user.userName) {
         return;
       }
@@ -39,13 +39,13 @@ export class ClientSignController implements ClientStateController {
       this.userNotificationsService.pushNotification({
         message: `There ${dto.numUsers} user${dto.numUsers === 1 ? '' : 's'} now`,
       });
-      this.clientModeMediatorService.setClientMode(ClientMode.WatchingMode);
+      this.clientModeStore.setClientMode(ClientMode.WatchingMode);
       this.clientStateStore.switchState(ClientState.Switching);
     });
   }
 
   public stop(): void {
-    this.userStateService.removeUserListener(this.listenerId);
+    this.userStore.removeUserListener(this.listenerId);
     this.listenerId = undefined;
   }
 }
