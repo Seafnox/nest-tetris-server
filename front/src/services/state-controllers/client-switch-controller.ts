6e4b7fd9d1024a6c36fca637cd09f69d1f94c008
Kirill Warp <kirill9990@gmail.com>
2021-01-08 18:27:33 +0300

    code switch controller

--- front/src/services/state-controllers/client-switch-controller.ts
+++ front/src/services/state-controllers/client-switch-controller.ts
@@ -1,9 +1,31 @@
+import { ClientMode } from '../../enums/client-mode';
+import { ClientState } from '../../enums/client-state';
+import { ClientModeStore } from '../client-mode-store';
+import { ClientStateStore } from '../client-state-store';
+import { InjectorService } from '../Injector-factory';
 import { ClientStateController } from './client-state-controller';
 
 export class ClientSwitchController implements ClientStateController {
+  private readonly clientModeMediatorService: ClientModeStore;
+  private readonly clientStateStore: ClientStateStore;
+  private listenerId: symbol;
+
+  constructor(injector: InjectorService) {
+    this.clientModeMediatorService = injector.inject(ClientModeStore);
+    this.clientStateStore = injector.inject(ClientStateStore);
+  }
   public start(): void {
+    this.listenerId = this.clientModeMediatorService.addClientModeListener(mode => {
+      switch (mode) {
+        case ClientMode.WatchingMode:
+          return this.clientStateStore.switchState(ClientState.Watching);
+        case ClientMode.PlayingMode:
+          return this.clientStateStore.switchState(ClientState.Playing);
+      }
+    })
   }
 
   public stop(): void {
+    this.clientModeMediatorService.removeClientModeListener(this.listenerId);
   }
 }
