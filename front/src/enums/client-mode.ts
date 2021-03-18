9a3aa3d05e5734d125646c3c6303874b7ea95dca
Kirill Warp <kirill9990@gmail.com>
2021-03-19 02:55:04 +0300

    add none client mode

--- front/src/enums/client-mode.ts
+++ front/src/enums/client-mode.ts
@@ -1,4 +1,5 @@
 export enum ClientMode {
+  None,
   PlayingMode,
   WatchingMode,
 }
--- front/src/enums/client-status.ts
+++ front/src/enums/client-status.ts
@@ -1,4 +1,4 @@
-export const enum ClientState {
+export const enum ClientStatus {
   Init = 'Init',
   Signing = 'Signing',
   Playing = 'Playing',
